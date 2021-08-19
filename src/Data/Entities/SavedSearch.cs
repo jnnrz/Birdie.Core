6652d751b570e69238625d1d7f04bf50d90f2b5a
Johann Perozo <johann.prz@gmail.com>
2021-08-19 12:06:44 -0400

    Fixes SavedSearch Id no longer ignored

--- src/Data/Entities/SavedSearch.cs
+++ src/Data/Entities/SavedSearch.cs
@@ -1,11 +1,9 @@
-using System.Text.Json.Serialization;
 using FluentValidation;
 
 namespace Birdie.Core.Data.Entities
 {
     public class SavedSearch
     {
-        [JsonIgnore]
         public long Id { get; set; }
         public string Search { get; set; }
         public long UserId { get; set; }
